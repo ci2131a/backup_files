412582b9b2303629eed55aae0d2824bb2aab1c55
Charles Ingulli <charlesfi@outlook.com>
2024-05-12 12:56:13 -0400

    Update R script for copy_and_delete() use

--- Scripts/Script.R
+++ Scripts/Script.R
@@ -2,8 +2,19 @@
 # sources the function file
 # and contains use example
 
+# backup_files.r
 
 # source
-source("Backup_Files.R")
+source("backup_files.R")
 # example
-Backup_Files("D:\\Source", "H:\\Backup")
+backup_files("D:\\Source", "H:\\Backup")
+
+
+# copy_and_delete.r
+
+# source functions
+source("copy_and_delete.R")
+# example
+source_directory <- "D:\\Source Directory"
+target_directory <- "D:\\Target Directory"
+copy_any_delete(source_directory, target_directory, output_console = FALSE, output_tree = FALSE, log_file = NULL)
